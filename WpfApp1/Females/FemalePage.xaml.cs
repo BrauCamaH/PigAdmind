35497e65985810759e0a6f8dd720211ab4ddf6c2
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-15 07:51:13 -0600

    Add female page template

---
+++ WpfApp1/Females/FemalePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Females
+{
+	/// <summary>
+	/// Interaction logic for FemalePage.xaml
+	/// </summary>
+	public partial class FemalePage : UserControl
+	{
+		public FemalePage()
+		{
+			InitializeComponent();
+		}
+
+		private void TabablzControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void TabablzControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void TabablzControl_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+	}
+}
