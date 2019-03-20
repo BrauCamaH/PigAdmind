5dec26927d34b3cc93604e4cfeae26a88c7e3c04
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-20 15:44:13 -0600

    Fix config problem

--- WpfApp1/Properties/Resources.Designer.cs
+++ WpfApp1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PigAdmindApp.Properties {
+namespace WpfApp1.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PigAdmindApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PigAdmindApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApp1.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
