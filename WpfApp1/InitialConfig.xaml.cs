714d341da8d4541dc239c53e0ad160275a2ab99e
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-25 09:59:29 -0600

    Add user to bdd

--- WpfApp1/InitialConfig.xaml.cs
+++ WpfApp1/InitialConfig.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.DatabaseFirst;
 
 namespace WpfApp1
 {
@@ -24,9 +25,41 @@ namespace WpfApp1
 		{
 			InitializeComponent();
 		}
-
+		private void AddUser(string email, String pass)
+		{
+			var context = new Entities();
+			var user = new Users()
+			{
+				email = email,
+				password = pass
+			};
+			context.Users.Add(user);
+			context.SaveChanges();
+		}
+		private bool ValidatePassword(String pass, string scnpass)
+		{
+			return pass.Equals(scnpass);
+		}
+		private void OpenLogIn()
+		{
+			var login = new Login();
+			login.Show();
+			Close();
+		}
 		private void Btn_Click(object sender, RoutedEventArgs e)
 		{
+			var email = EmailBox.Text;
+			var pass = PasswordBox.Password;
+			var scnpass = PasswordBox_Copy.Password;
+			if (email != "" && pass != "" && ValidatePassword(pass, scnpass))
+			{
+				AddUser(email, pass);
+				OpenLogIn();
+			}
+			else
+			{
+				MessageBox.Show("Ingrese todos los datos de forma correcta");
+			}
 
 		}
 	}
