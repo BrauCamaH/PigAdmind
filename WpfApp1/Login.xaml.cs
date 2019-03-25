6acf7b98e14af1153ff5331512749337c0ab97d1
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-25 10:01:40 -0600

    Validate user password

--- WpfApp1/Login.xaml.cs
+++ WpfApp1/Login.xaml.cs
@@ -11,22 +11,50 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.DatabaseFirst;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for Login.xaml
-    /// </summary>
-    public partial class Login : Window
-    {
-        public Login()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for Login.xaml
+	/// </summary>
+	public partial class Login : Window
+	{	
+		public Login()
+		{
+			InitializeComponent();
+		}
+		
+		private bool IsTheCorrectPassword(string inputPass)
+		{
+			var ctx = new Entities();
+			var user = ctx.Users.SqlQuery("select * from users").First<Users>();
+			
+			var pass = user.password.Replace(" ","");
+			if (pass==inputPass)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		
+		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-
+			if (IsTheCorrectPassword(PasswordBox.Password))
+			{
+				var window = new MainWindow();
+				window.Show();
+				Close();
+			}
+			else
+			{
+				MessageBox.Show("La contraseña es incorrecta");
+			}
+	
 		}
 	}
 }
