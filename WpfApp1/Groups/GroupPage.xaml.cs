dfb722180172a4676a17ff640abba69727a43326
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-25 10:26:06 -0600

    Add grouppage template

---
+++ WpfApp1/Groups/GroupPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Groups
+{
+	/// <summary>
+	/// Interaction logic for GroupPage.xaml
+	/// </summary>
+	public partial class GroupPage : UserControl
+	{
+		public GroupPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
