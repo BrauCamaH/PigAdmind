eb94d174b32dd13425abc06e8d62852f760cbb8a
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-25 09:57:09 -0600

    Manipulate initial window by code

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfApp1.DatabaseFirst;
 
 namespace WpfApp1
 {
@@ -13,5 +14,27 @@ namespace WpfApp1
 	/// </summary>
 	public partial class App : Application
 	{
+		private bool IsAUserAtDatabase()
+		{
+			var ctx = new Entities();
+			var user = ctx.Users.SqlQuery("Select * from Users ").FirstOrDefault<Users>();
+			if (user != null)
+			{
+				return true;
+			}
+
+			return false;
+		}
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			if (IsAUserAtDatabase())
+			{
+				new Login().Show();
+			}
+			else
+			{
+				new InitialConfig().Show();
+			}
+		}
 	}
 }
