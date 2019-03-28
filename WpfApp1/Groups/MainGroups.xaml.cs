eb08cda76a096c8f7ab7d74c7a95b603df25c902
Braulio Camarena <braulio.camarenah@gmail.com>
2019-03-28 17:04:55 -0600

    Add a new group

--- WpfApp1/Groups/MainGroups.xaml.cs
+++ WpfApp1/Groups/MainGroups.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp1.DatabaseFirst;
 
 namespace WpfApp1.Groups
 {
@@ -10,11 +14,55 @@ namespace WpfApp1.Groups
 		public MainGroups()
 		{
 			InitializeComponent();
+			GetGroupsFromDataBase();
+		}
+		private bool IsDataComplete(string txtbox, string datePicker)
+		{
+			if (txtbox != "" && datePicker != "")
+			{
+				return true;
+			}
+			return false;
+		}
+		private void GetGroupsFromDataBase()
+		{
+			var ctx = new Entities();
+			GroupList.ItemsSource = ctx.PigGroups.ToList();
+		}
+
+		private void AddNewGroup(int n, string date)
+		{
+			var ctx = new Entities();
+			var pigGroup = new DatabaseFirst.PigGroups()
+			{
+				weaning_date = date,
+				weigth_avg = 0,
+				second_avg = 0,
+				pig_count = n,
+				died_pigs = 0,
+				user = 1
+			};
+			ctx.PigGroups.Add(pigGroup);
+			ctx.SaveChanges();
 		}
 
 		private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 
 		}
+
+		private void AddNewGroup_OnClick(object sender, RoutedEventArgs e)
+		{
+			int nPigs = Int32.Parse(NPigsBox.Text);
+			string weanigDate = DatePicker.Text;
+			if (IsDataComplete(NPigsBox.Text, weanigDate))
+			{
+				AddNewGroup(nPigs, weanigDate);
+				GetGroupsFromDataBase();
+			}
+
+		}
+
+
 	}
 }
