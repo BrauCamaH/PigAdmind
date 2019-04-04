91f6d2bc223fe56869e73430968ace332ad7f7cb
Braulio Camarena <braulio.camarenah@gmail.com>
2019-04-04 17:45:35 -0600

    Add delete template

---
+++ WpfApp1/Delete.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for Delete.xaml
+	/// </summary>
+	public partial class Delete : UserControl
+	{
+		public Delete()
+		{
+			InitializeComponent();
+		}
+
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			Btn_Aceptar.IsEnabled = true;
+		}
+
+		private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			Btn_Aceptar.IsEnabled = true;
+		}
+	}
+}
