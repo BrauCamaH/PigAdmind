160b7acdf710c4bc163a819f4fa174a4654be62d
Braulio Camarena <braulio.camarenah@gmail.com>
2019-04-05 18:01:27 -0600

    Show editdialog by contextmanager

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using MaterialDesignThemes.Wpf;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using WpfApp1.DatabaseFirst;
@@ -22,6 +23,7 @@ namespace WpfApp1
 			MenuToolbarManager.Back = BackBtn;
 			MenuToolbarManager.Edit = EditBtn;
 			MenuToolbarManager.Delete = DeleteBtn;
+			ContextManager.Instance().CurrentControlContext = new EditFemale();
 		}
 
 		private void SetInitialPage()
@@ -51,6 +53,7 @@ namespace WpfApp1
 		private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			UserControl usc = null;
+
 			GridMain.Children.Clear();
 			int index = ListViewMenu.SelectedIndex;
 			MoveCursorMenu(index);
@@ -60,14 +63,17 @@ namespace WpfApp1
 				case "Females":
 					usc = new MainFemales();
 					GridMain.Children.Add(usc);
+					ContextManager.Instance().CurrentControlContext = new EditFemale();
 					break;
 				case "Groups":
 					usc = new MainGroups();
 					GridMain.Children.Add(usc);
+					ContextManager.Instance().CurrentControlContext = new EditGroup();
 					break;
 				case "Sales":
 					usc = new MainSales();
 					GridMain.Children.Add(usc);
+					ContextManager.Instance().CurrentControlContext = new EditSale();
 					break;
 			}
 		}
@@ -104,7 +110,7 @@ namespace WpfApp1
 
 		private void EditBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			EditDialogHost.IsOpen = true;
 		}
 
 		private void DeleteBtn_Click(object sender, RoutedEventArgs e)
@@ -118,6 +124,17 @@ namespace WpfApp1
 		}
 
 		private void DeleteDialogHost_DialogClosing(object sender, MaterialDesignThemes.Wpf.DialogClosingEventArgs eventArgs)
+		{
+			Window.IsEnabled = true;
+		}
+
+		private void EditDialogHost_DialogOpened(object sender, DialogOpenedEventArgs eventargs)
+		{
+			EditGrid.Children.Add(ContextManager.Instance().CurrentControlContext);
+			Window.IsEnabled = false;
+		}
+
+		private void EditDialogHost_DialogClosing(object sender, DialogClosingEventArgs eventargs)
 		{
 			Window.IsEnabled = true;
 		}
---
+++ WpfApp1/Managers/ContextManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Managers
+{
+	class ContextManager
+	{
+		public static ContextManager Instance()
+		{
+			if (_instance == null)
+			{
+				lock (Padlock)
+				{
+					if (_instance == null)
+					{
+						_instance = new ContextManager();
+					}
+				}
+			}
+			return _instance;
+		}
+
+
+		private static ContextManager _instance = null;
+		private static readonly object Padlock = new object();
+
+		public UserControl CurrentControlContext { get; set; }
+		public int CurrentElementSelected { get; set; }
+	}
+}
