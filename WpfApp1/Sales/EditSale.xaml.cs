42b3e2ccae1f0c8568d7d8c9d848bd0f872081bb
Braulio Camarena <braulio.camarenah@gmail.com>
2019-04-10 15:57:10 -0500

    Add checbox to editsale

--- WpfApp1/Sales/EditSale.xaml.cs
+++ WpfApp1/Sales/EditSale.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1.Sales
 {
-    /// <summary>
-    /// Interaction logic for EditSale.xaml
-    /// </summary>
-    public partial class EditSale : UserControl
-    {
-        public EditSale()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for EditSale.xaml
+	/// </summary>
+	public partial class EditSale : UserControl
+	{
+		public EditSale()
+		{
+			InitializeComponent();
+		}
+
+		private void CheckBox_checked(object sender, RoutedEventArgs e)
+		{
+			Accept_btn.IsEnabled = true;
+		}
+
+		private void CheckBox_unchecked(object sender, RoutedEventArgs e)
+		{
+			Accept_btn.IsEnabled = false;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Confirm_checkbox.IsChecked = false;
+		}
+	}
 }
