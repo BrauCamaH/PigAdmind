efe977b700e148d1043290d4f226607982e73d15
Braulio Camarena <braulio.camarenah@gmail.com>
2019-04-10 15:50:00 -0500

    Add checkbox to editfemale

--- WpfApp1/Females/EditFemale.xaml.cs
+++ WpfApp1/Females/EditFemale.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1.Females
 {
-    /// <summary>
-    /// Interaction logic for EditFemale.xaml
-    /// </summary>
-    public partial class EditFemale : UserControl
-    {
-        public EditFemale()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for EditFemale.xaml
+	/// </summary>
+	public partial class EditFemale : UserControl
+	{
+		public EditFemale()
+		{
+			InitializeComponent();
+		}
+
+		private void CheckBox_checked(object sender, RoutedEventArgs e)
+		{
+			Accept_btn.IsEnabled = true;
+		}
+
+		private void CheckBox_unchecked(object sender, RoutedEventArgs e)
+		{
+			Accept_btn.IsEnabled = false;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Confirm_checkbox.IsChecked = false;
+		}
+	}
 }
