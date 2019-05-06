a33f2b80856411acd77947de612fa2dd1de26610
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-06 07:43:35 -0500

    Add editinsemination

---
+++ WpfApp1/Females/InseminationViews/EditInsemination.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Females.InseminationViews
+{
+	/// <summary>
+	/// Interaction logic for EditInsemination.xaml
+	/// </summary>
+	public partial class EditInsemination : UserControl
+	{
+		public EditInsemination()
+		{
+			InitializeComponent();
+		}
+		private void CheckBox_checked(object sender, RoutedEventArgs e)
+		{
+			AcceptBtn.IsEnabled = true;
+		}
+
+		private void CheckBox_unchecked(object sender, RoutedEventArgs e)
+		{
+			AcceptBtn.IsEnabled = false;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			ConfirmCheckbox.IsChecked = false;
+		}
+	}
+}
