42c3e32565d4abbd3919977d1b501b054acd0ee8
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-06 07:47:55 -0500

    Add editsick

---
+++ WpfApp1/Females/SickViews/EditSick.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Females.SickViews
+{
+	/// <summary>
+	/// Interaction logic for EditSick.xaml
+	/// </summary>
+	public partial class EditSick : UserControl
+	{
+		public EditSick()
+		{
+			InitializeComponent();
+		}
+	}
+}
