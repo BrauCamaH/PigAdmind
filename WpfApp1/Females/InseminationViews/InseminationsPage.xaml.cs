85c687435904aeb3dc99439d594954bc56250cc7
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-14 12:33:20 -0500

    Add inseminationspage

---
+++ WpfApp1/Females/InseminationViews/InseminationsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Females.InseminationViews
+{
+    /// <summary>
+    /// Lógica de interacción para InseminationsPage.xaml
+    /// </summary>
+    public partial class InseminationsPage : UserControl
+    {
+        public InseminationsPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
