008ee34f2426ff561381c2b5d1708e70d1a40821
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-15 20:46:04 -0500

    Add looktextbox

---
+++ WpfApp1/CustomUserControls/LookTextBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp1.CustomUserControls
+{
+    /// <summary>
+    /// Lógica de interacción para LookTextBox.xaml
+    /// </summary>
+    public partial class LookTextBox : UserControl
+    {
+        private CollectionView _view;
+        private ListView _listView;
+        public LookTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public void SetView(ListView view, Predicate<object> filter)
+        {
+            _listView = view;
+            _view = (CollectionView)CollectionViewSource.GetDefaultView(view.ItemsSource);
+            _view.Filter = filter;
+        }
+
+        private void LookButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = "";
+        }
+
+
+        private void TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(_listView.ItemsSource).Refresh();
+        }
+    }
+}
