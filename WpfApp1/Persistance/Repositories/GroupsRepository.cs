d82f24ec23bab2ca0bdfa7a8c532daf3298379ae
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-21 16:27:56 -0500

    Implement groups repository

---
+++ WpfApp1/Persistance/Repositories/GroupsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Data.Entity;
+using System.Linq;
+using WpfApp1.Core.Repositories;
+using WpfApp1.DatabaseFirst;
+using WpfApp1.Persistance.Repositories;
+
+namespace WpfApp1.Persistance
+{
+    class GroupsRepository : Repository<GroupsRepository>, IGroupsRepository
+    {
+        public GroupsRepository(DbContext cont) : base(cont)
+        {
+        }
+
+        public IEnumerable GetFemalesByUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PigGroups GetGroupById(int id)
+        {
+            return DbEntities.PigGroups.FirstOrDefault(g => g.id == id);
+        }
+
+        public void RemoveGroupById(int id)
+        {
+            var query = from p in DbEntities.PigGroups
+                        where p.id == id
+                        select p;
+            if (query.First() != null)
+            {
+                DbEntities.PigGroups.Remove(query.FirstOrDefault() ?? throw new InvalidOperationException());
+            }
+        }
+
+        public Entities DbEntities => Context as Entities;
+    }
+}
--- WpfApp1/Persistance/UnitOfWork.cs
+++ WpfApp1/Persistance/UnitOfWork.cs
@@ -5,32 +5,39 @@ using WpfApp1.Persistance.Repositories;
 
 namespace WpfApp1.Persistance
 {
-	class UnitOfWork : IUnitOfWork
-	{
-		private readonly Entities _context;
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly Entities _context;
 
-		public UnitOfWork(Entities context)
-		{
-			_context = context;
-			Births = new BirthsRepository(_context);
-			Females = new FemalesRepository(_context);
-		}
+        public UnitOfWork(Entities context)
+        {
+            _context = context;
 
-		public void Dispose()
-		{
-			_context.Dispose();
-		}
+            Births = new BirthsRepository(_context);
+            Females = new FemalesRepository(_context);
+            Inseminations = new InseminationsRepository(_context);
+            Births = new BirthsRepository(_context);
+            Sicks = new SicksRepository(_context);
+            Groups = new GroupsRepository(_context);
+        }
 
-		public IFemalesRepository Females { get; private set; }
-		public IInseminationsRepository Inseminations { get; private set; }
-		public IBirthsRepository Births { get; private set; }
-		public ISicksRepository Sicks { get; private set; }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
 
-		public int Complete()
-		{
-			return _context.SaveChanges();
-		}
+        public IFemalesRepository Females { get; private set; }
+        public IInseminationsRepository Inseminations { get; private set; }
+        public IBirthsRepository Births { get; private set; }
+        public ISicksRepository Sicks { get; private set; }
+
+        public IGroupsRepository Groups { get; private set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
 
 
-	}
+    }
 }
