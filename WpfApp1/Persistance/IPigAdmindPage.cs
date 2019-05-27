d49d935f21569188e39169197b0753127781aeb6
Braulio Camarena <braulio.camarenah@gmail.com>
2019-05-27 08:43:11 -0500

    Add pigadmindpage interface

---
+++ WpfApp1/Persistance/IPigAdmindPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfApp1.Persistance
+{
+    interface IPigAdmindPage<TEntity, TEventArgs> where TEntity : class
+    {
+        void InitializeCrudControls(TEntity entity);
+        bool CustomFilter(Object obj);
+
+        void GetItemsFromDatabase();
+
+        void OnItemAdded(object sender, TEventArgs e);
+
+        void OnItemDeleted(object sender, TEventArgs e);
+
+        void OnItemEdited(object sender, TEventArgs e);
+    }
+}
