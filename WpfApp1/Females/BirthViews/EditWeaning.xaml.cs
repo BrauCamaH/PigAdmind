e51254657f3d0bfdb28a00988bb256dc1c814763
Braulio Camarena <braulio.camarenah@gmail.com>
2019-06-05 16:58:37 -0500

    Add edit a weaning

---
+++ WpfApp1/Females/BirthViews/EditWeaning.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Females.BirthViews
+{
+    /// <summary>
+    /// Lógica de interacción para EditWeaning.xaml
+    /// </summary>
+    public partial class EditWeaning : UserControl
+    {
+        public EditWeaning()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
