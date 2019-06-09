ad26051e9a531f4204e37cfbf25d59c33acd56d9
Braulio Camarena <braulio.camarenah@gmail.com>
2019-06-09 12:37:46 -0500

    Add customdatepicker

---
+++ WpfApp1/CustomUserControls/CustomDatePicker.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.CustomUserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomDatePicker.xaml
+    /// </summary>
+    public partial class CustomDatePicker : DatePicker
+    {
+        public CustomDatePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
