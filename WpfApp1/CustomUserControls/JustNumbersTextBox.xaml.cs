10da3a3065955a7b674c0ec260e447d7d35a85a9
Braulio Camarena <braulio.camarenah@gmail.com>
2019-06-09 11:45:18 -0500

    Add  Custom TextBoxs

---
+++ WpfApp1/CustomUserControls/JustNumbersTextBox.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1.CustomUserControls
+{
+    /// <summary>
+    /// Interaction logic for JustNumbersTextBox.xaml
+    /// </summary>
+    public partial class JustNumbersTextBox : TextBox
+    {
+        public JustNumbersTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ WpfApp1/CustomUserControls/NotEmptyTextBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.CustomUserControls
+{
+    /// <summary>
+    /// Interaction logic for NotEmptyTextBox.xaml
+    /// </summary>
+    public partial class NotEmptyTextBox : TextBox
+    {
+        public NotEmptyTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
